6eac67efadf51f956f6b63b3cdbc1cadafc0a235
Tram <tramnhoang@gmail.com>
2024-09-16 22:32:19 -0700

    Deal cards activity

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 SUITS = ["♥", "♦", "♠", "♣︎"]
 HAND_SIZE = 10
 
+        
 def build_deck():
-    return [f"{card}{suit}" for card in CARDS for suit in SUITS]
+    return[f"{card}{suit}" for card in CARDS for suit in SUITS]
 
-# write a function that deals 10 cards from a deck of cards without using the sample function
 def deal_ten_cards(deck_of_cards):
-    pass
+    dealt_cards = []
+    indices_used = set()
+    
+    while len(dealt_cards) < HAND_SIZE:
+        index = random.randint(0, len(deck_of_cards) -1)
+        if index not in indices_used:
+            dealt_cards.append(deck_of_cards[index])
+            indices_used.add(index)
+    return dealt_cards
+
+deck = build_deck()
+hand = deal_ten_cards(deck)
+print("Dealt hand:", hand)
